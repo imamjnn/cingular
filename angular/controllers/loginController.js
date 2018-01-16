41cdd3cf9d003c36e8d13b5fd8aad0654ddc407b
Reorocker7 <rerocker7@gmail.com>
2018-01-16 23:03:50 +0700

    Update controller with session

--- angular/controllers/loginController.js
+++ angular/controllers/loginController.js
@@ -1,33 +1,39 @@
 var app = angular.module('mainApp');
 
 app.controller('loginCtrl', function($scope, $http, $location, $window){
-	$scope.isLoading = false;
-	$scope.user = {
-		username: '',
-		password: ''
-	};
-	$scope.onSubmit = function(){
-		$scope.isLoading = true;
-		var login = {
-			method: 'POST',
-			url: '/api/auth/check_login',
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			data: {
-				'username': $scope.user.username,
-				'password': $scope.user.password
-			}
+	var token = $window.localStorage.getItem('token');
+	if(token){
+		$location.url('/user');
+	}else{
+		$scope.isLoading = false;
+		$scope.user = {
+			username: '',
+			password: ''
 		};
-		$http(login).then(function(res){
-			$scope.isLoading = false;
-			if(res.data.status === 'success'){
-				console.log('login berhasil, selanjutnya terserah anda');
-				$window.localStorage.setItem('token', res.data.token);
-				$location.url('/user');
-			}else{
-				console.log('gagal '+res);
-			}
-		});
+		$scope.onSubmit = function(){
+			$scope.isLoading = true;
+			var login = {
+				method: 'POST',
+				url: '/api/auth/check_login',
+				headers: {
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				data: {
+					'username': $scope.user.username,
+					'password': $scope.user.password
+				}
+			};
+			$http(login).then(function(res){
+				$scope.isLoading = false;
+				if(res.data.status === 'success'){
+					console.log('login berhasil, selanjutnya terserah anda');
+					$window.localStorage.setItem('token', res.data.token);
+					$location.url('/user');
+				}else{
+					console.log('gagal '+res);
+				}
+			});
+		}
 	}
+	
 })
--- angular/controllers/userController.js
+++ angular/controllers/userController.js
@@ -1,40 +1,50 @@
 var app = angular.module('mainApp');
 
 app.controller('userCtrl', function($scope, $location, $http, $routeParams, $window){
-	//$window.localStorage.setItem('jajal', 'oke');
+	
 	var token = $window.localStorage.getItem('token');
-	var get_user = {
-		method: 'GET',
-		url: '/api/user/getuser',
-		headers: {
-			Authorization: token
-		}
-	};
-	$http(get_user)
-	.then(function(res){
-		$scope.users = res.data.users;
-		console.log(res);
-	});
-
-	$scope.refresh = function(){
-		$http.get('/api/user/getuser')
+	if(token == null){
+		$location.url('/login');
+	}else{
+		var get_user = {
+			method: 'GET',
+			url: '/api/user/getuser',
+			headers: {
+				Authorization: token
+			}
+		};
+		$http(get_user)
 		.then(function(res){
 			$scope.users = res.data.users;
-		})
-	};
+			console.log(res);
+		});
 
-	$scope.delUser = function(id){
-		var del = {
-			method: 'DELETE',
-			url: '/api/user/deleteuser/'+id
-		}
-		var c = confirm('Are you sure?');
-		if(c == true){
-			$http(del).then(function(res){
-				$scope.refresh();
+		$scope.refresh = function(){
+			$http.get('/api/user/getuser')
+			.then(function(res){
+				$scope.users = res.data.users;
 			})
+		};
+
+		$scope.delUser = function(id){
+			var del = {
+				method: 'DELETE',
+				url: '/api/user/deleteuser/'+id
+			}
+			var c = confirm('Are you sure?');
+			if(c == true){
+				$http(del).then(function(res){
+					$scope.refresh();
+				})
+			}
+			
+		};
+
+		$scope.logOut = function() {
+			$window.localStorage.removeItem('token');
+			console.log('logout man');
 		}
-		
 	}
+	
 
 });
